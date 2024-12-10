8a248a71c86e76e72dfb809d7372e8dc857af38b
Nico Gomez <nicog851@gmail.com>
2024-12-10 09:05:22 -0300

    segundo commit

--- myapp/views.py
+++ myapp/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    context = {"mensaje": "Bienvenidos a mi aplicacion Django"}
+    context = {"mensaje": "Bienvenidos a mi aplicacion Django 2"}
     return render(request, "myapp/index.html", context)
